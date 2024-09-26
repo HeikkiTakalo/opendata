575c9a8bc4e7abbd099bed6797fedbd769e7b36a
HeikkiTakalo <heikkitakalo@windowslive.com>
2024-09-26 14:34:24 +0300

    juu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 function App() {
 
+  async function getData() {
+    const response = await fetch('https://yesno.wtf/api');
+    const jsonBody = await response.json()
+    console.log(jsonBody.answer);
+  }
 
+  function getData2() {
+    fetch('https://yesno.wtf/api')
+      .then(resp => resp.json()
+        .then(json => console.log(json.answer))
+      )
+  }
 
   return (
     <div>
-
+      <button onClick={getData}>OK</button>
     </div>
   )
 }
